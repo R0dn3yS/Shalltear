af39ad2a0f7e46241694b571996d2153123aeeb8
Rodney van den Velden <rodneymichael2002@gmail.com>
2022-12-19 17:46:18 +0100

    Fix me being a idiot

--- src/commands/util/arch.js
+++ src/commands/util/arch.js
@@ -2,7 +2,7 @@ module.exports = {
   name: 'arch',
   description: 'Search archwiki',
   run: async (_client, message, args) => {
-    const url = 'https://wiki.archlinux.org/index.php?search=';
+    const url = 'https://wiki.archlinux.org/index.php?search=' + args.join('+');
     const newUrl = await fetch(url).then(res => res.url);
     return message.channel.send(newUrl);
   }
